28c8b2f3fbc85e4db0bf83c5dfd0c778cc3adfe6
Luluh <luluhaltayyar@gmail.com>
2024-05-08 14:29:41 +0300

    update

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class HomePage {
+
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    LogInPage log = new LogInPage();
+
+
+    @FindBy(id = "link6")
+    public WebElement remoteUnitIcon ;
+
+    public void remoteUnitClick()
+    {
+        log.defaultLogin();
+        //Click on "Remote Units" icon on the side menu.
+        remoteUnitIcon.click();
+    }
 
 }
