85a7e20a4b054c41cd82cd5f80f41e621d269f52
123Alshaker <161316739+123Alshaker@users.noreply.github.com>
2024-05-13 13:54:37 +0300

    all comminte added

--- src/test/java/pages/UserModuleProcess.java
+++ src/test/java/pages/UserModuleProcess.java
@@ -1,81 +1,100 @@
 package pages;
 
-import com.github.javafaker.Faker;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
-import utilities.ReusableMethods;
+import com.github.javafaker.Faker; // Import JavaFaker library for generating fake data
+import org.openqa.selenium.Keys; // Import Keys class for keyboard actions
+import org.openqa.selenium.WebElement; // Import WebElement for web element interactions
+import org.openqa.selenium.interactions.Actions; // Import Actions class for performing user actions
+import org.openqa.selenium.support.FindBy; // Import FindBy annotation for locating elements
+import org.openqa.selenium.support.PageFactory; // Import PageFactory for initializing elements
+import utilities.Driver; // Import Driver class for WebDriver instance
+import utilities.ReusableMethods; // Import ReusableMethods class for reusable actions
 
-import java.util.List;
-
+import java.util.List; // Import List interface for handling lists
 
 public class UserModuleProcess {
 
-
-
-
-
+    // Constructor to initialize elements
     public UserModuleProcess() {
-        PageFactory.initElements(Driver.getDriver(), this);
+        PageFactory.initElements(Driver.getDriver(), this); // Initialize elements using PageFactory
     }
 
+    // Web elements annotated with FindBy
     @FindBy(xpath = "//*[@alt='Collapse']")
     public WebElement button;
+
     @FindBy(xpath = "//a[normalize-space()='Users']")
     public WebElement userButton;
+
     @FindBy(xpath = "//button[normalize-space()='+ Add New Member']")
     public WebElement addNewButton;
+
     @FindBy(xpath = "//*[@class = ' css-19bb58m']")
     public WebElement firstToDropdown;
+
     @FindBy(xpath = "//*[contains(text(),'Select Role')]/following-sibling::div")
     public WebElement sacondToDropdown;
+
     @FindBy(xpath = "//input[@id='email']")
     public WebElement emailInput;
+
     @FindBy(xpath = "//button[normalize-space()='Register']")
     public WebElement registerButton;
+
     @FindBy(xpath = "//div[@class='toast-header bg-success text-white']")
     public List<WebElement> successMasseg;
+
     @FindBy(xpath = "//button[normalize-space()='Close']")
     public WebElement cancelButton;
+
     @FindBy(xpath = "//tbody/tr[1]/td[7]/div[1]/button[1]")
     public WebElement thereDots;
+
     @FindBy(xpath = "//div[@id = 'MainContent']//table/tbody/tr[1]/td[2]/a")
     public WebElement firstUsernameIndex;
+
     @FindBy(xpath = "//span[@class='active-roles-box']")
     public WebElement defaultRole;
+
     @FindBy(xpath = "//button[@class='btn btn-ghost-dark rounded-circle']//*[name()='svg']")
     public WebElement editIcon;
+
     @FindBy(xpath = "//label[@id='email']")
     public WebElement emailLabel;
+
     @FindBy(xpath = "//input[@id='username']")
     public WebElement usernameLabel;
+
     @FindBy(xpath = "(//a[@class='dropdown-item'][normalize-space()='Reset Password'])[1]")
     public WebElement resetPasswordButton;
+
     @FindBy(xpath = "//button[normalize-space()='Confirm']")
     public WebElement confirmButtom;
+
     @FindBy(xpath = "//div[@class='callout callout-success']")
     public List<WebElement> resetPassSuccessMass;
+
     @FindBy(xpath = "//button[normalize-space()='Close']")
     public WebElement closeButtonResetPass;
+
     @FindBy(css = ".ms-2.cursor-pointer")
-     public WebElement addingRole;
+    public WebElement addingRole;
+
     @FindBy(xpath = "//div[@class=' css-19bb58m']")
     public WebElement drobdownRols;
+
     @FindBy(xpath = "//button[normalize-space()='Save']")
     public WebElement saveButton;
+
     @FindBy(xpath = "(//*[name()='svg'][@role='img'])[15]")
     public WebElement checkMark;
+
     @FindBy(xpath = "//p[normalize-space()='New role added for this user successfuly']")
     public List<WebElement> addingRoleSuccessMass;
 
 
 
-
-    public void scrollByThreeElements(){
-
+    // Method to scroll and choose an element
+    public void scrollByThreeElements() {
         Actions actions = new Actions(Driver.getDriver());
         ReusableMethods.waitFor(2);
         drobdownRols.click();
@@ -85,43 +104,30 @@ public class UserModuleProcess {
                 .sendKeys(Keys.ENTER)
                 .build()
                 .perform();
-
     }
 
+    // Method to scroll without clicking and choose an element (used for adding users)
+    public void scrollByThreeElementsInAddingUser() {
+        Actions actions = new Actions(Driver.getDriver());
+        ReusableMethods.waitFor(2);
+        actions.sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .build()
+                .perform();
+    }
 
-    public String fakeEmail(){
+    // Method to generate a fake email using JavaFaker library
+    public String fakeEmail() {
         Faker faker = new Faker();
         return faker.internet().emailAddress();
-
     }
 
-    public String fakeUsername(){
+    // Method to generate a fake username using JavaFaker library
+    public String fakeUsername() {
         Faker faker = new Faker();
         return faker.name().username();
-
     }
 
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/test/java/tests/userModuleProcess/US00017.java
+++ src/test/java/tests/userModuleProcess/US00017.java
@@ -1,39 +1,63 @@
 package tests.userModuleProcess;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import pages.LogInPage;
-import pages.UserModuleProcess;
-import utilities.ReusableMethods;
+
+import org.testng.Assert; // Import TestNG assertion methods
+import org.testng.annotations.Test; // Import TestNG annotation for test methods
+import pages.LogInPage; // Import LogInPage class for logging in
+import pages.UserModuleProcess; // Import UserModuleProcess class for user module actions
+import utilities.ReusableMethods; // Import ReusableMethods class for reusable actions
 
 public class US00017 {
 
-
+    // Create instances of required page classes
     LogInPage logInPage = new LogInPage();
     UserModuleProcess moduleProcess = new UserModuleProcess();
 
+    // Test method to assert the successful addition of a new user with correct email
     @Test
     public void AsserTheNewlyUserAdding() throws InterruptedException {
-        //WithCorrectEmail
-
+        // Login with default credentials
         logInPage.defaultLogin();
+
+        // Click on the collapse button
         moduleProcess.button.click();
-        ReusableMethods.waitForClickablility(moduleProcess.button,2).click();
-        ReusableMethods.waitFor(5);
+
+        // Click on the user button after waiting for it to be clickable
+        ReusableMethods.waitForClickablility(moduleProcess.userButton, 2).click();
+
+        // Click on the add new button
         moduleProcess.addNewButton.click();
-        ReusableMethods.waitFor(5);
+
+        // Wait for 2 seconds
+        ReusableMethods.waitFor(2);
+
+        // Click on the first dropdown
         moduleProcess.firstToDropdown.click();
-        moduleProcess.scrollByThreeElements();
+
+        // Scroll and choose an option from the dropdown for the first dropdown
+        moduleProcess.scrollByThreeElementsInAddingUser();
+
+        // Click on the first dropdown again
         moduleProcess.firstToDropdown.click();
+
+        // Wait for 5 seconds
         ReusableMethods.waitFor(5);
+
+        // Click on the second dropdown
         moduleProcess.sacondToDropdown.click();
-        moduleProcess.scrollByThreeElements();
+
+        // Scroll and choose an option from the dropdown for the second dropdown
+        moduleProcess.scrollByThreeElementsInAddingUser();
+
+        // Enter a fake email address
         moduleProcess.emailInput.sendKeys(moduleProcess.fakeEmail());
+
+        // Wait for 5 seconds
         ReusableMethods.waitFor(5);
+
+        // Click on the register button
         moduleProcess.registerButton.click();
+
+        // Assert that the success message is displayed indicating the user was added successfully
         Assert.assertTrue(moduleProcess.successMasseg.size() > 0);
-
-
     }
-
-
 }
